3cdb11509e53ae47493f784444b87b3d96f7c6fe
Adams Banjo <sajioloye@gmail.com>
2025-02-17 17:37:33 -0500

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -3,9 +3,10 @@ import path from 'node:path';
 import zlib from 'node:zlib';
 import crypto from 'node:crypto';
 import {
+    createReadStream,
     createWriteStream,
 } from 'node:fs';
-import { pipeline, Readable } from 'node:stream'
+import { pipeline, Readable, Writable } from 'node:stream'
 
 const args = process.argv.slice(2);
 const command = args[0];
@@ -13,7 +14,15 @@ const command = args[0];
 enum Commands {
     Init = "init",
     CatFile = "cat-file",
-    HashObject = "hash-object"
+    HashObject = "hash-object",
+    ListTree = "ls-tree"
+}
+
+interface TreeOutput {
+    mode: string;
+    treeType: 'tree' | 'blob';
+    sha: string;
+    name: string;
 }
 
 switch (command) {
@@ -35,14 +44,18 @@ switch (command) {
         if (catFlag !== '-p' || !blob) {
             throw new Error(`Incomplete cmd; Pass a flag or blob key`);
         }
-        const objectDir = blob.slice(0, 2);
-        const blobString = blob.slice(2);
 
-        const blobData = fs.readFileSync(path.resolve('.git', 'objects', objectDir, blobString))
-        const decompressed = zlib.inflateSync(new Uint8Array(blobData)).toString();
-        // strip out 'blob' and number of chars and end of line
-        const content = decompressed.replace(/^blob \d+\0/, '').trim();
-        process.stdout.write(content);
+        const { directory: objectDir, filename: blobString } = splitSha(blob);
+        // const blobData = fs.readFileSync(path.resolve('.git', 'objects', objectDir, blobString))
+        // const decompressed = zlib.inflateSync(new Uint8Array(blobData)).toString();
+        const filePath = path.resolve('.git', 'objects', objectDir, blobString);
+
+        decompressFile(filePath, zlib.createInflate()).then(result => {
+            const content = result.replace(/^blob \d+\0/, '').trim();
+            process.stdout.write(content);
+        }).catch(err => {
+            console.error(`err: ${err}`)
+        })
         break;
 
     case Commands.HashObject:
@@ -57,8 +70,7 @@ switch (command) {
         const blobHeader = `blob ${fileContents.length}\0`;
         const objectContent = `${blobHeader}${fileContents}\n`.trim()
         const sha = crypto.createHash('sha1').update(objectContent).digest('hex');
-        const newObjDir = sha.slice(0, 2);
-        const objFileName = sha.slice(2);
+        const { directory: newObjDir, filename: objFileName } = splitSha(sha);
         // Create directory if it doesn't exist
         const objectPath = path.resolve('.git', 'objects', newObjDir);
         fs.mkdirSync(objectPath, { recursive: true });
@@ -73,6 +85,90 @@ switch (command) {
         })
         break;
 
+    case Commands.ListTree:
+        const [, listFlag, listHash] = args;
+        if ((listFlag && listFlag !== '--name-only') || !listHash) {
+            throw new Error(`Invalid command arguments`);
+        }
+
+        if (listHash.length !== 40) {
+            throw new Error(`Invalid blob sha`);
+        }
+        // take the sha and split into directory and sha file
+        const { directory: treeDir, filename: treeFile } = splitSha(listHash);
+        // read the file and decompress
+        const inflate = zlib.createInflate();
+        const blobPath = path.resolve(`.git`, 'objects', treeDir, treeFile);
+
+        //* start of method to decompress using readfile
+        // const treePath = fs.readFileSync(path.resolve('.git', 'objects', treeDir, treeFile))
+        // const treeData = zlib.inflateSync(new Uint8Array(treePath)).toString();
+
+        // const directories = treeData.split('\0')
+        //     .slice(1, -1)
+        //     .filter(element => element.split(' ').length > 1)
+        //     .map(element => element.split(' ').at(-1))
+        //     .join('\n');
+
+        //* end of decompress method using readFile
+
+        decompressFile(blobPath, inflate).then(result => {
+            const directories = result.split('\0')
+                .slice(1, -1)
+                .filter(element => element.split(' ').length > 1)
+                .map(element => element.split(' ').at(-1))
+                .join('\n');
+
+            if (listFlag) {
+                process.stdout.write(`${directories}\n`)
+            }
+        }).catch(err => {
+            console.error(`err: ${err}`)
+        })
+
+        break;
     default:
         throw new Error(`Unknown command ${command}`);
 }
+
+/**
+ * Conforms with the git object directory strucure
+ * @param sha 40-character sha1
+ * @returns directory and filename
+ */
+function splitSha(sha: string): { directory: string, filename: string } {
+    return {
+        directory: sha.slice(0, 2),
+        filename: sha.slice(2)
+    };
+}
+
+/**
+ * Decompresses a blob file via file streaming
+ * @param inputSource 
+ * @param transformer 
+ * @param destination 
+ * @returns decompressed data
+ */
+function decompressFile(inputSource: string, transformer: zlib.Inflate): Promise<string> {
+    const source = createReadStream(inputSource);
+    let result = '';
+
+    const writer = new Writable({
+        write(chunk: Buffer, _encoding, callback) {
+            result += chunk.toString();
+            callback();
+        }
+    });
+
+    return new Promise((resolve, reject) => {
+        pipeline(source, transformer, writer, err => {
+            if (err) reject(err);
+            resolve(result)
+        })
+    })
+}
+
+
+//TODO: implement full tree response
+//TODO: move implementation into separate file
